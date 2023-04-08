6a84d7017a95aa336a15900568fb38738b1e7057
Kirill Boblak <kirill.boblak2004@mail.ru>
2023-04-08 10:41:19 +0300

    Создал структуру файлов

---
+++ Testing System Programming/Teacher.cpp
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Teacher.h"
+
+using namespace std;
+
+void Teacher()
+{
+
+}
--- Testing System Programming/Testing System Programming.cpp
+++ Testing System Programming/Testing System Programming.cpp
@@ -1,1 +1,44 @@
-﻿
+﻿#include <iostream>
+#include <locale>
+#include "Students.h"
+#include "Teacher.h"
+
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	setlocale(LC_ALL, "Rus");
+	int choice;
+	do
+	{
+		system("cls");
+		cout << " Система тестирования по курсу программирование" << endl;
+		cout << " =============================================" << endl;
+		cout << " Выберите режим работы: " << endl;
+		cout << " 1. Режим преподавателя" << endl;
+		cout << " 2. Режим студента" << endl;
+		cout << " 0. Выход из программы" << endl;
+		cout << endl;
+		cout << " Выберите режим: ";
+		cin >> choice;
+		switch (choice)
+		{
+		case 1:
+			Teacher();
+			break;
+		case 2:
+			Students();
+			break;
+		case 0:
+			cout << "Выход из программы." << endl;
+			break;
+		default:
+			system("cls");
+			cout << " Ошибка: неверный пункт меню. Попробуйте ещё раз!" << endl << endl;
+			break;
+		}
+	} while (choice != 0);
+	
+	return 0;
+}
